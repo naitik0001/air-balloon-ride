07234511cb20ad72f30702265941f1c456367113
vishu warrior <87360076+naitik0001@users.noreply.github.com>
2022-01-20 18:01:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,149 +1,76 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-var engine, world, backgroundImg, waterSound, backgroundMusic, cannonExplosion;
-var canvas, angle, tower, ground, cannon, boat;
-var balls = [];
-var boats = [];
+var balloon,balloonImage1,balloonImage2;
+var database;
+var height;
 
-var boatAnimation = [];
-var boatSpritedata, boatSpritesheet;
+function preload(){
+   bg =loadImage("Images/cityImage.png");
+   balloonImage1=loadAnimation("Images/HotAirBallon-01.png");
+   balloonImage2=loadAnimation("Images/HotAirBallon-01.png","Images/HotAirBallon-01.png",
+   "Images/HotAirBallon-01.png","Images/HotAirBallon-02.png","Images/HotAirBallon-02.png",
+   "Images/HotAirBallon-02.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png");
+  }
 
-var brokenBoatAnimation = [];
-var brokenBoatSpritedata, brokenBoatSpritesheet;
-
-function preload() {
-  backgroundImg = loadImage("./assets/background.gif");
-  towerImage = loadImage("./assets/tower.png");
-  boatSpritedata = loadJSON("assets/boat/boat.json");
-  boatSpritesheet = loadImage("assets/boat/boat.png");
-  brokenBoatSpritedata = loadJSON("assets/boat/broken_boat.json");
-  brokenBoatSpritesheet = loadImage("assets/boat/broken_boat.png");
-}
-
+//Function to set initial environment
 function setup() {
-  canvas = createCanvas(1200,600);
-  engine = Engine.create();
-  world = engine.world;
-  angle = -PI / 4;
-  ground = new Ground(0, height - 1, width * 2, 1);
-  tower = new Tower(150, 350, 160, 310);
-  cannon = new Cannon(180, 110, 100, 50, angle);
+  database=firebase.database();
+  createCanvas(1500,700);
 
-  var boatFrames = boatSpritedata.frames;
-  for (var i = 0; i < boatFrames.length; i++) {
-    var pos = boatFrames[i].position;
-    var img = boatSpritesheet.get(pos.x, pos.y, pos.w, pos.h);
-    boatAnimation.push(img);
-  }
+  balloon=createSprite(250,650,250,650);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
 
-  var brokenBoatFrames = brokenBoatSpritedata.frames;
-  for (var i = 0; i < brokenBoatFrames.length; i++) {
-    var pos = brokenBoatFrames[i].position;
-    var img = brokenBoatSpritesheet.get(pos.x, pos.y, pos.w, pos.h);
-    brokenBoatAnimation.push(img);
-  }
+  var balloonHeight=database.ref('balloon/height');
+  balloonHeight.on("value",readHeight, console.log("error"));
+  textSize(20); 
 }
 
+// function to display UI
 function draw() {
-  background(189);
-  image(backgroundImg, 0, 0, width, height);
+  background(bg);
 
-
-  Engine.update(engine);
-  ground.display();
-
-  showBoats();
-
- 
-  for (var i = 0; i < balls.length; i++) {
-    showCannonBalls(balls[i], i);
-    for (var j = 0; j < boats.length; j++) {
-      if (balls[i] !== undefined && boats[j] !== undefined) {
-        var collision = Matter.SAT.collides(balls[i].body, boats[j].body);
-        if (collision.collided) {
-          boats[j].remove(j);
-
-          Matter.World.remove(world, balls[i].body);
-          balls.splice(i, 1);
-          i--;
-          
-        }
-      } 
-    }
+  if(keyDown(LEFT_ARROW)){
+    updateHeight(-10,0);
+    //add the animation of balloon [use balloonImage2]
   }
-  cannon.display();
-  tower.display();
+  else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    //add the animation of balloon [use balloonImage2]
+  }
+  else if(keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+ //add the animation of balloon [use balloonImage2]
+    balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    updateHeight(0,+10);
+  //add the animation of balloon [use balloonImage2]
+    balloon.scale=balloon.scale+0.005;
+  }
 
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
 
 }
 
 
-//creating the cannon ball on key press
-function keyPressed() {
-  if (keyCode === DOWN_ARROW) {
-    var cannonBall = new CannonBall(cannon.x, cannon.y);
-    cannonBall.trajectory = [];
-    Matter.Body.setAngle(cannonBall.body, cannon.angle);
-    balls.push(cannonBall);
-  }
-}
-
-// function to show the ball.
-function showCannonBalls(ball, index) {
-  ball.display();
-  if (ball.body.position.x >= width || ball.body.position.y >= height - 50) {
-    Matter.World.remove(world, ball.body);
-    balls.splice(index, 1);
-  }
+function updateHeight(x,y){
+  database.ref('/balloon/height').update({
+    'x': height.x + x ,
+    'y': height.y + y
+  })
 }
 
 
-//function to show the boat
-function showBoats() {
-  if (boats.length > 0) {
-    if (
-      boats.length < 4 &&
-      boats[boats.length - 1].body.position.x < width - 300
-    ) {
-      var positions = [-40, -60, -70, -20];
-      var position = random(positions);
-      var boat = new Boat(
-        width,
-        height - 100,
-        170,
-        170,
-        position,
-        boatAnimation
-      );
 
 
-      boats.push(boat);
-    }
+function readHeight(data){
+  //assign the value of data to height
+  //assign the x and y value of height to the respective x and y position of balloon
+ }
 
-    for (var i = 0; i < boats.length; i++) {
-      Matter.Body.setVelocity(boats[i].body, {
-        x: -0.9,
-        y: 0
-      });
-
-      boats[i].display();
-      boats[i].animate();
-     
-    }
-  } else {
-    var boat = new Boat(width, height - 60, 170, 170, -60, boatAnimation);
-    boats.push(boat);
-  }
+function showError(){
+  console.log("Error in writing to the database");
 }
-
-
-//releasing the cannonball on key release
-function keyReleased() {
-  if (keyCode === DOWN_ARROW) {
-    balls[balls.length - 1].shoot();
-  }
-}
-
-
